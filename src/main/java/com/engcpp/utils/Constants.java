e35b8e10a7d7133200127b8ab91db3adcd3a9435
engcpp <engcpp@yahoo.com.br>
2018-06-04 18:44:01 +1200

    Property IVAL Report

---
+++ src/main/java/com/engcpp/utils/Constants.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.engcpp.utils;
+
+/**
+ *
+ * @author engcpp
+ */
+public class Constants {
+    public static final String IQC_URL = "https://localhost";
+    public static final String USERNAME = "myusername";
+    public static final String PASSWORD = "mypassword";
+}
